504d07552af27394582a333812999274b03126a0
Mat Moore <mat.moore@digital.cabinet-office.gov.uk>
2016-09-27 11:28:06 +0100

    Filter out content we don't know about
    When building lists of content tagged to a taxon,
    skip the ones we don't have html for.

---
+++ lib/ruby/taxon_document_fetcher.rb
@@ -0,0 +1,33 @@
+class TaxonDocumentFetcher
+  attr_reader :all_documents
+
+  def initialize(all_documents)
+    @all_documents = Set.new(all_documents)
+  end
+
+  def fetch_for_taxon(taxon_content_id)
+    response = GdsApi.with_retries(maximum_number_of_attempts: 2) do
+      DataImport.rummager.search(
+        filter_taxons: taxon_content_id,
+        fields: %w(title description link format public_timestamp),
+        count: 1000
+      ).to_h
+    end
+
+    before_size = response["results"].size
+
+    response = filter_response(response)
+
+    after_size = response["results"].size
+    puts "filtered out #{before_size - after_size} results for #{taxon_content_id}"
+
+    response
+  end
+
+private
+
+  def filter_response(response)
+    response["results"] = response["results"].select { |result| all_documents.include? result["link"] }
+    response
+  end
+end
