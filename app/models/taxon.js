c816feacd4a2f3217148224a8e946f77d66a0167
Mo Baig <mo@mobaig.com>
2016-11-10 11:46:16 +0000

    Remove unnecessary IIFE from taxon.js plus linting

--- app/models/taxon.js
+++ app/models/taxon.js
@@ -1,129 +1,120 @@
 var ContentItem = require("./content_item.js");
 
-/*
-  Object to describe the structure of a taxon
-  (its child taxons and the documents tagged to it)
-*/
-(function() {
-  "use strict";
-
-  class Taxon {
-    constructor(title, basePath, description, options) {
-      this.title = title;
-      this.basePath = basePath;
-      this.description = description;
-      if (options === undefined) {
-        options = {};
-      }
-      if (options.content === undefined) {
-        options.content = [];
-      }
-      if (options.children === undefined) {
-        options.children = [];
-      }
-      this.content = options.content;
-      this.children = options.children;
+class Taxon {
+  constructor(title, basePath, description, options) {
+    this.title = title;
+    this.basePath = basePath;
+    this.description = description;
+    if (typeof(options) === "undefined") {
+      options = {};
     }
-
-    filterByHeading(headingId) {
-      var filteredContent = this.content.filter(
-        function(child) {
-          var heading = child.getHeading();
-          return heading !== null && heading.id === headingId;
-        }
-      );
-      var filteredTaxon = new Taxon(
-        this.title, this.basePath, this.description,
-        {content: filteredContent, children: this.children}
-      );
-      return filteredTaxon;
-    }
-
-    addContent(content) {
-      this.content.push(content);
-    }
-
-    addChild(taxon) {
-      this.children.push(taxon);
-    }
-
-    popularContent(maxDocuments) {
-      return this.content.slice(0, maxDocuments);
-    }
-
-    atozContent(maxDocuments) {
-      var sorted = this.content.sort(function(a, b) {
-        if (a.title > b.title) {
-          return 1;
-        }
-
-        if (a.title < b.title) {
-          return -1;
-        }
-
-        return 0;
-      });
-
-      if (maxDocuments !== undefined) {
-        return sorted.slice(0, maxDocuments);
-      } else {
-        return sorted;
-      }
-    }
-
-    recentContent(maxDocuments) {
-      return this.content.sort(function(a, b) {
-        return b.publicTimestamp.getTime() - a.publicTimestamp.getTime();
-      }).slice(0, maxDocuments);
-    }
-
-    atozChildren() {
-      return this.children.sort(function(a, b) {
-        return a.title > b.title;
-      });
-    }
-
-    static fromMetadata(basePath, metadata) {
-      var taxonInformation = metadata.taxon_information[basePath];
-      if (taxonInformation === undefined) {
-        console.log("Missing taxon information for %s", basePath);
-        return null;
-      }
-
-      var taxon = new Taxon(taxonInformation.title, basePath, taxonInformation.description);
-      var contentItems = metadata.documents_in_taxon[basePath].results;
-      var childTaxons = metadata.children_of_taxon[basePath];
-
-      contentItems.forEach(function(contentItem) {
-        var documentType = metadata.document_metadata[contentItem.link.substring(1)].document_type;
-        var publicTimestamp = contentItem.public_timestamp;
-
-        // Manual sections are missing a public timestamp: make one up
-        if (publicTimestamp === undefined) {
-            publicTimestamp = '2016-01-01T00:00:00+00:00';
-        }
-
-        var contentItemModel = new ContentItem(
-          contentItem.title,
-          contentItem.link,
-          documentType,
-          new Date(publicTimestamp),
-          contentItem.description
-        );
-        taxon.addContent(contentItemModel);
-      });
-
-      childTaxons.forEach(function(childTaxonBasePath) {
-        var childTaxon = Taxon.fromMetadata(childTaxonBasePath, metadata);
-        if (childTaxon !== null) {
-          taxon.addChild(childTaxon);
-        }
-      });
-
-      return taxon;
-    }
+    this.content = options.content || [];
+    this.children = options.children || [];
   }
 
-  module.exports = Taxon;
+  filterByHeading (headingId) {
+    var filteredContent = this.content.filter(
+      function (child) {
+        var heading = child.getHeading();
 
-})();
+        return heading !== null && heading.id === headingId;
+      }
+    );
+    var filteredTaxon = new Taxon(
+      this.title, this.basePath, this.description,
+      {
+        content: filteredContent,
+        children: this.children
+      }
+    );
+
+    return filteredTaxon;
+  }
+
+  addContent (content) {
+    this.content.push(content);
+  }
+
+  addChild (taxon) {
+    this.children.push(taxon);
+  }
+
+  popularContent (maxDocuments) {
+    return this.content.slice(0, maxDocuments);
+  }
+
+  atozContent (maxDocuments) {
+    var sorted = this.content.sort(function (a, b) {
+      if (a.title > b.title) {
+        return 1;
+      }
+
+      if (a.title < b.title) {
+        return -1;
+      }
+
+      return 0;
+    });
+
+    if (typeof(maxDocuments) !== "undefined") {
+      return sorted.slice(0, maxDocuments);
+    }
+
+    return sorted;
+  }
+
+  recentContent (maxDocuments) {
+    return this.content.sort(function (a, b) {
+      return b.publicTimestamp.getTime() - a.publicTimestamp.getTime();
+    }).slice(0, maxDocuments);
+  }
+
+  atozChildren () {
+    return this.children.sort(function (a, b) {
+      return a.title > b.title;
+    });
+  }
+
+  static fromMetadata (basePath, metadata) {
+    var taxonInformation = metadata.taxon_information[basePath];
+    if (typeof(taxonInformation) === "undefined") {
+      console.log("Missing taxon information for %s", basePath);
+
+      return null;
+    }
+
+    var taxon = new Taxon(taxonInformation.title, basePath, taxonInformation.description);
+    var contentItems = metadata.documents_in_taxon[basePath].results;
+    var childTaxons = metadata.children_of_taxon[basePath];
+
+    contentItems.forEach(function (contentItem) {
+      var documentType = metadata.document_metadata[contentItem.link.substring(1)].document_type;
+      var publicTimestamp = contentItem.public_timestamp;
+
+      // Manual sections are missing a public timestamp: make one up
+      if (typeof(publicTimestamp) === "undefined") {
+          publicTimestamp = '2016-01-01T00:00:00+00:00';
+      }
+
+      var contentItemModel = new ContentItem(
+        contentItem.title,
+        contentItem.link,
+        documentType,
+        new Date(publicTimestamp),
+        contentItem.description
+      );
+      taxon.addContent(contentItemModel);
+    });
+
+    childTaxons.forEach(function (childTaxonBasePath) {
+      var childTaxon = Taxon.fromMetadata(childTaxonBasePath, metadata);
+      if (childTaxon !== null) {
+        taxon.addChild(childTaxon);
+      }
+    });
+
+    return taxon;
+  }
+}
+
+module.exports = Taxon;
