d863cd50886fc284c8a7b124affbd2187e76087e
Alex Torrance <hello@alextorrance.co.uk>
2017-01-24 19:11:40 +0000

    Truncate long breadcrumbs and add toggle to show hidden crumbs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,15 +3,21 @@
   var $ = global.jQuery;
   var GOVUK = global.GOVUK || {};
 
-$(function () {
+  $(function () {
 
-  GOVUK.modules.start();
-  var $section = $('.subsection__header');
-  if ($section.length === 1) {
-      $section.trigger('click');
-  }
+    GOVUK.modules.start();
+    var $section = $('.subsection__header');
+    if ($section.length === 1) {
+        $section.trigger('click');
+    }
 
-  GOVUK.modules.start();
+    GOVUK.modules.start();
 
-});
+    $('.js-toggle-breadcrumb').on('click', function(e){
+      e.preventDefault();
+
+      $(this).parent().hide().siblings('.js-hidden').removeClass('js-hidden');
+    })
+
+  });
 })(window);
