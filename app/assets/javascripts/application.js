ec595ab1136a8e19409f8ec45cf4d3e0299a2c59
Alex Torrance <hello@alextorrance.co.uk>
2017-01-26 18:40:31 +0000

    Refactor breadcrumb truncation
    Makes sense to move the logic to client side for progressive enhancement and cleaner code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,30 @@
 
     GOVUK.modules.start();
 
+    var truncateBreadcrumbs = function() {
+      var maxBreadcrumb = 4;
+      var $breadcrumbs = $('.breadcrumbs').find('li');
+      if ($breadcrumbs.length > maxBreadcrumb) {
+        $breadcrumbs.each(function(i){
+          var $this = $(this);
+          if (i > 1 && i < $breadcrumbs.length - 2) {
+            $this.hide();
+          }
+        });
+        var x = $breadcrumbs.eq(1).after('<li><a href="" class="js-toggle-breadcrumb" title="Show breadcrumbs">…</a></li>');
+        console.log(x);
+      }
+    }
+
+    truncateBreadcrumbs();
+
     $('.js-toggle-breadcrumb').on('click', function(e){
       e.preventDefault();
 
-      $(this).parent().hide().siblings('.js-hidden').removeClass('js-hidden');
-    })
+      $(this).parent().hide().siblings(':hidden').show();
+    });
+
+
 
   });
 })(window);
