7b9e72f6c57c27ba22956aae8bced27fa376ef62
Carlos Vilhena <carlosvilhena@gmail.com>
2017-01-31 11:54:48 +0000

    Fetch related links from ElasticSearch
    This commit populates the related links via Rummager.

--- app/models/related_content.js
+++ app/models/related_content.js
@@ -1,17 +1,90 @@
 var fs = require('fs');
+var process = require('process');
+var https = require('https');
+var querystring = require('querystring');
 var Promise = require('bluebird');
+var TaxonomyData = require('./taxonomy_data.js');
 var readFile = Promise.promisify(fs.readFile);
+var Taxon = require('./taxon.js');
 
 class RelatedContent {
-  static get (contentBasePath) {
-    var readSourceData = readFile('app/data/hardcoded_related_content.json');
+  static esRelatedLinks (contentBasePath, parentTaxon) {
+    var queryParams = {
+      similar_to: contentBasePath,
+      start: 0,
+      count: 5,
+      filter_taxons: [parentTaxon],
+      fields: 'title,description,link'
+    }
+    var queryString = querystring.stringify(queryParams);
+    var path = "/api/search.json?" + queryString;
+
+    return new Promise((resolve, reject) =>
+      https.get({
+        host: 'www-origin.integration.publishing.service.gov.uk',
+        auth: process.env.INT_CREDENTIALS,
+        path: path,
+      }, function (response) {
+        var body = '';
+
+        response.on('data', function (d) {
+            body += d;
+        });
+
+        response.on('end', function () {
+          var parsed = JSON.parse(body);
+          var data = parsed.results;
+
+          resolve(data);
+        });
+
+        response.on('error', reject);
+      })
+    );
+  }
+
+  static allTaxonsPromise () {
+    var readSourceData = readFile('app/data/taxons.json');
 
     return readSourceData.
       then(function (data) {
-        var lookup = JSON.parse(data);
-
-        return lookup[contentBasePath];
+        return JSON.parse(data);
       });
+  }
+
+  static get (contentBasePath) {
+    var that = this;
+    var allTaxons = this.allTaxonsPromise();
+    var parentTaxonsPromise = allTaxons.then(function (taxons) {
+      return TaxonomyData.get().
+        then(function (metadata) {
+          return metadata.taxons_for_content[contentBasePath.slice(1)].map(function (taxonBasePath) {
+            var parentTaxon = taxons.find(function (taxon) {
+              return taxon.base_path == taxonBasePath;
+            });
+            var contentItem = Taxon.fromMetadata(taxonBasePath, metadata);
+            contentItem.contentId = parentTaxon.content_id;
+
+            return contentItem;
+          })
+        });
+    });
+
+    return parentTaxonsPromise.then(function (parentTaxons) {
+      var searchResults = parentTaxons.map(function (parentTaxon) {
+        return that.esRelatedLinks(contentBasePath, parentTaxon.contentId).then(function (results) {
+          return {
+            results: results,
+            basePath: parentTaxon.basePath,
+            title: parentTaxon.title,
+            contentId: parentTaxon.contentId,
+            description: parentTaxon.description
+          };
+        });
+      });
+
+      return Promise.all(searchResults);
+    });
   }
 }
 
