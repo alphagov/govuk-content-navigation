e8582c87784b2b960ab20ffef44b3e7ea02064fe
Carlos Vilhena <carlosvilhena@gmail.com>
2017-02-15 16:15:07 +0000

    Add model to represent guidance content
    This will allow us to re-use the concept of "guidance" across the code.

---
+++ app/models/guidance_content.js
@@ -0,0 +1,35 @@
+"use strict";
+
+class GuidanceContent {
+  static guidanceDocumentTypes () {
+    return [
+      'answer',
+      'contact',
+      'detailed_guidance',
+      'detailed_guide',
+      'form',
+      'guidance',
+      'guide',
+      'licence',
+      'local_transaction',
+      'manual',
+      'manual_section',
+      'map',
+      'place',
+      'programme',
+      'promotional',
+      'regulation',
+      'simple_smart_answer',
+      'smartanswer',
+      'statutory_guidance',
+      'transaction',
+      'travel-advice'
+    ];
+  }
+
+  static isGuidanceContent (documentType) {
+    return (this.guidanceDocumentTypes().indexOf(documentType) > 0);
+  }
+}
+
+module.exports = GuidanceContent;
