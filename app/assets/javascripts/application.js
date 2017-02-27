1a940cffae940920b734679e853f8f260091fce8
Alex Torrance <hello@alextorrance.co.uk>
2017-02-27 15:34:22 +0000

    Only show related links for primary topic
    No longer need the JS reveal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,21 +14,4 @@
     GOVUK.modules.start();
 
   });
-
-  $(function() {
-    $('.related-topic-links').each(function(i) {
-      if (i > 0) {
-        var $this = $(this);
-        var title = $this.find('h2').text();
-        $this.find('ul').hide();
-        $this.find('.reveal').text('See related links in '+title).on('click', function(e) {
-          e.preventDefault();
-          $this.find('ul').show();
-          $(this).text('See everything in '+title).on('click', function(e) {
-            window.location = this.href;
-          });
-        })
-      }
-    })
-  })
 })(window);
