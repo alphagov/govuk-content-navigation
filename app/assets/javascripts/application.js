e288a93dfdd1f7df805d4f8478d1ba37e80f9837
Caroline Green <caroline.green@digital.cabinet-office.gov.uk>
2017-03-13 10:24:32 +0000

    Bump govuk elements and add radio buttons
    Bump govuk elements to pull in the new radio button styles.  Add templating logic to work with radio buttons.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,16 @@
     }
 
     GOVUK.modules.start();
-
   });
+
+  // Use GOV.UK selection-buttons.js to set selected
+  // and focused states for block labels
+  var $blockLabels = $(".block-label input[type='radio'], .block-label input[type='checkbox']")
+  new GOVUK.SelectionButtons($blockLabels) // eslint-disable-line
+
+  // Where .block-label uses the data-target attribute
+  // to toggle hidden content
+  var showHideContent = new GOVUK.ShowHideContent()
+  showHideContent.init()
+
 })(window);
