391a2053b11154bab28862196b70cf9f83dffb62
Carlos Vilhena <carlosvilhena@gmail.com>
2017-04-19 11:06:06 +0100

    Fix lint errors in routes.js

--- app/routes.js
+++ app/routes.js
@@ -7,91 +7,91 @@ var ContentPresenter = require('./models/content_presenter.js');
 var GuidanceContent = require('./models/guidance_content.js');
 var SearchService = require('./models/search_service');
 
-  router.get('/', function (req, res) {
-    DocumentTypes.examples()
-      .then(function (examples) {
-        var guidanceExamples = examples.filter(function (example) {
-          return GuidanceContent.isGuidanceContent(example.documentType);
-        });
-
-        res.render('index', {
-          documentTypeExamples: guidanceExamples
-        });
+router.get('/', function (req, res) {
+  DocumentTypes.examples().
+    then(function (examples) {
+      var guidanceExamples = examples.filter(function (example) {
+        return GuidanceContent.isGuidanceContent(example.documentType);
       });
-  });
 
-  router.get('/education/:taxon?', function (req, res) {
-    var taxonParam = req.params.taxon;
-
-    if(taxonParam == undefined) {
-      taxonParam = "/education";
-    }
-    else {
-      taxonParam = "/education/" + taxonParam;
-    }
-    var viewAll = !(typeof(req.query.viewAll) === "undefined");
-
-    var taxonPresenter = new TaxonPresenter(taxonParam);
-    taxonPresenter.build().then(presentTaxon);
-
-    function presentTaxon(presentedTaxon) {
-      if (viewAll) {
-        var backTo = presentedTaxon.determineBackToLink(req.url);
-        res.render('taxonomy/view-all', {
-          presentedTaxon: presentedTaxon,
-          backTo: backTo,
-        });
-
-        return;
-      }
-
-      if (presentedTaxon.isPenultimate) {
-        res.render('taxonomy/penultimate-taxon', {
-          presentedTaxon: presentedTaxon,
-        });
-      } else {
-        res.render('taxonomy/taxon', {
-          presentedTaxon: presentedTaxon,
-        });
-      }
-    }
-  });
-
-  router.get('/home/?', function(req, res) {
-    res.render('home');
-  });
-
-  router.get('/search', function(req, res) {
-    var scopedSearch = SearchService.scopedSearch(
-      req.query.q,
-      req.query.scope
-    );
-
-    var allGovUkResultCount = SearchService.count(req.query.q);
-
-    Promise.all([scopedSearch, allGovUkResultCount])
-      .then(function (promiseResolution) {
-        res.render('search', {
-          queryParams: req.query,
-          scopedSearch: promiseResolution[0],
-          allGovUkResultsCount: promiseResolution[1]
-        });
+      res.render('index', {
+        documentTypeExamples: guidanceExamples
       });
-  });
+    });
+});
 
-  router.get(/\/.+/, function (req, res) {
-    var basePath = req.url;
-    const contentPresenter = new ContentPresenter(basePath)
+router.get('/education/:taxon?', function (req, res) {
+  var taxonParam = req.params.taxon;
 
-    contentPresenter.present().
-      then(function (presentedContent) {
-        res.render('content', {
-          presentedContent: presentedContent,
-        })
-      }).
-      catch(function () {
-        res.status(404).render('404');
+  if(!taxonParam) {
+    taxonParam = "/education";
+  }
+  else {
+    taxonParam = "/education/" + taxonParam;
+  }
+  var viewAll = !(typeof(req.query.viewAll) === "undefined");
+
+  var taxonPresenter = new TaxonPresenter(taxonParam);
+  taxonPresenter.build().then(presentTaxon);
+
+  function presentTaxon (presentedTaxon) {
+    if (viewAll) {
+      var backTo = presentedTaxon.determineBackToLink(req.url);
+      res.render('taxonomy/view-all', {
+        presentedTaxon: presentedTaxon,
+        backTo: backTo,
+      });
+
+      return;
+    }
+
+    if (presentedTaxon.isPenultimate) {
+      res.render('taxonomy/penultimate-taxon', {
+        presentedTaxon: presentedTaxon,
+      });
+    } else {
+      res.render('taxonomy/taxon', {
+        presentedTaxon: presentedTaxon,
+      });
+    }
+  }
+});
+
+router.get('/home/?', function (req, res) {
+  res.render('home');
+});
+
+router.get('/search', function (req, res) {
+  var scopedSearch = SearchService.scopedSearch(
+    req.query.q,
+    req.query.scope
+  );
+
+  var allGovUkResultCount = SearchService.count(req.query.q);
+
+  Promise.all([scopedSearch, allGovUkResultCount]).
+    then(function (promiseResolution) {
+      res.render('search', {
+        queryParams: req.query,
+        scopedSearch: promiseResolution[0],
+        allGovUkResultsCount: promiseResolution[1]
+      });
+    });
+});
+
+router.get(/\/.+/, function (req, res) {
+  var basePath = req.url;
+  const contentPresenter = new ContentPresenter(basePath)
+
+  contentPresenter.present().
+    then(function (presentedContent) {
+      res.render('content', {
+        presentedContent: presentedContent,
       })
-  });
+    }).
+    catch(function () {
+      res.status(404).render('404');
+    })
+});
 
-  module.exports = router;
+module.exports = router;
