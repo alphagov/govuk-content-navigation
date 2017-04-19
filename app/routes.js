1cc642bbc69927eda930ef79ed0809404ff25ea7
Carlos Vilhena <carlosvilhena@gmail.com>
2017-04-19 13:12:50 +0100

    Move site routes (/ and /home) into own file

--- app/routes.js
+++ app/routes.js
@@ -4,23 +4,10 @@ var router = express.Router();
 var SearchRoutes = require('./routes/search');
 var TaxonRoutes = require('./routes/taxons');
 var ContentItemRoutes = require('./routes/content_items');
+var SiteRoutes = require('./routes/site');
 
-var DocumentTypes = require('./models/document_types.js');
-
-router.get('/', function (req, res) {
-  DocumentTypes.guidanceExamples().
-    then(function (guidanceExamples) {
-      res.render(
-        'index',
-        {documentTypeExamples: guidanceExamples}
-      );
-  });
-});
-
-router.get('/home/?', function (req, res) {
-  res.render('home');
-});
-
+router.get('/', SiteRoutes.index);
+router.get('/home/?', SiteRoutes.home);
 router.get('/search', SearchRoutes.show);
 router.get('/:theme/:taxon?', TaxonRoutes.show);
 router.get(/\/.+/, ContentItemRoutes.show);
